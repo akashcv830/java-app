9c3d81f66861fb5350ea885cedef89ef5826e487
akashcv830 <akash.cv@hcltech.com>
2026-05-19 08:23:57 +0530

    Update App.java

--- java-app/src/main/java/com/example/App.java
+++ java-app/src/main/java/com/example/App.java
@@ -2,13 +2,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import
   org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.nind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
 public class App {
-public static void main(String[] args) {
-  SpringApplication.run(App.class, args);
-}
 public String home() {
   return " succesfully running";
+}
+  public static void main(String[] args) {
+  SpringApplication.run(App.class, args);
 }
 }
